0c2158f913676804de577f6c3ea816025e33e5b9
joaossr <joao53317@gmail.com>
2026-09-07 16:01:13 -0300

    Corrigir chave pública do Firebase

--- habitos-local/firebase-config.js
+++ habitos-local/firebase-config.js
@@ -1,7 +1,7 @@
 // Configuração pública do aplicativo Web Firebase.
 // As regras do Firestore e o Firebase Authentication protegem os dados.
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB6SagNB9roa9e3iFoLnB8mO11l2es",
+  apiKey: "AIzaSyB6SagNB9roUjaVKZA3iFoLnB8mO11l2es",
   authDomain: "meus-habitos-3c9b1.web.app",
   projectId: "meus-habitos-3c9b1",
   storageBucket: "meus-habitos-3c9b1.firebasestorage.app",
