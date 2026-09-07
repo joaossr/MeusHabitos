89c1bf6bd351bc98de1f506c0f7363fbc5845526
joaossr <joao53317@gmail.com>
2026-09-07 16:01:09 -0300

    Configurar Firebase Hosting como authDomain

--- habitos-local/firebase-config.js
+++ habitos-local/firebase-config.js
@@ -1,8 +1,8 @@
 // Configuração pública do aplicativo Web Firebase.
 // As regras do Firestore e o Firebase Authentication protegem os dados.
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB6SagNB9roUjaVKZA3iFoLnB8mO11l2es",
-  authDomain: "meus-habitos-3c9b1.firebaseapp.com",
+  apiKey: "AIzaSyB6SagNB9roa9e3iFoLnB8mO11l2es",
+  authDomain: "meus-habitos-3c9b1.web.app",
   projectId: "meus-habitos-3c9b1",
   storageBucket: "meus-habitos-3c9b1.firebasestorage.app",
   messagingSenderId: "600070990474",
