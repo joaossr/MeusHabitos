e715ce14a19b1f66fa77122fdaa351562f760deb
joaossr <joao53317@gmail.com>
2026-09-07 16:04:34 -0300

    Corrigir authDomain para OAuth do Firebase

--- habitos-local/firebase-config.js
+++ habitos-local/firebase-config.js
@@ -2,7 +2,7 @@
 // As regras do Firestore e o Firebase Authentication protegem os dados.
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyB6SagNB9roUjaVKZA3iFoLnB8mO11l2es",
-  authDomain: "meus-habitos-3c9b1.web.app",
+  authDomain: "meus-habitos-3c9b1.firebaseapp.com",
   projectId: "meus-habitos-3c9b1",
   storageBucket: "meus-habitos-3c9b1.firebasestorage.app",
   messagingSenderId: "600070990474",
